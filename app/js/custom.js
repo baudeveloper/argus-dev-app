dd95e3707d1e190f3dfa21e9dc1ff18383fe125b
baudeveloper <webmaster@baustein.ca>
2018-03-02 16:37:04 -0500

    Added a sample client login page

--- app/js/custom.js
+++ app/js/custom.js
@@ -9,8 +9,11 @@
 $(function() {
 	var siteBanner = $("[data-js=\"site-header\"]");
 	var imgSrc = siteBanner.find(".site--header-img").attr("src");
-	// siteBanner.attr("src", imgSrc);
 	siteBanner.css("backgroundImage", "url("+imgSrc+")");
+
+	var siteCompactBanner = $("[data-js=\"site-banner\"]");
+	var imgCompactSrc = siteCompactBanner.find(".site--banner-img").attr("src");
+	siteCompactBanner.css("backgroundImage", "url("+imgCompactSrc+")");
 })();
 
 // Start your scripts here please...
