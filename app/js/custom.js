030d0c12db5ab73df87fdfff5c63db6c6e43b9f2
baudeveloper <webmaster@baustein.ca>
2018-03-02 14:17:49 -0500

    Removed div in PUG files

--- app/js/custom.js
+++ app/js/custom.js
@@ -6,5 +6,11 @@
 //=============
 // jQuery Logic
 //=============
+$(function() {
+	var siteBanner = $("[data-js=\"site-header\"]");
+	var imgSrc = siteBanner.find(".site--header-img").attr("src");
+	// siteBanner.attr("src", imgSrc);
+	siteBanner.css("backgroundImage", "url("+imgSrc+")");
+})();
 
 // Start your scripts here please...
