96f25275d615754d8b5cdd72920ef50af50ed7ce
baudeveloper <webmaster@baustein.ca>
2018-03-05 15:46:59 -0500

    Added script to toggle tabs in Modal

--- app/js/custom.js
+++ app/js/custom.js
@@ -14,6 +14,16 @@ $(function() {
 	var siteCompactBanner = $("[data-js=\"site-banner\"]");
 	var imgCompactSrc = siteCompactBanner.find(".site--banner-img").attr("src");
 	siteCompactBanner.css("backgroundImage", "url("+imgCompactSrc+")");
+
+	// $("[data-js=\"modal-team-member\"]").click(function(e) {
+	// 	var tab = e.target.hash;
+	// 	$("li > a[href=\"" + tab + "\"]").tab("show");
+	// });
+
+	$("a[data-toggle=modal][data-target]").click(function() {
+		var target = $(this).attr("href");
+		$("a[data-toggle=tab][href=" + target + "]").tab("show");
+	});
 })();
 
 // Start your scripts here please...
